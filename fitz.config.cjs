6a3ce97a870436a85bd0e95ec8976738f9a53e3d
Fitzgeral <fitzgeralmedia@gmail.com>
2022-12-03 16:30:54 +0700

    new project

---
+++ fitz.config.cjs
@@ -0,0 +1,25 @@
+export default {
+  siteTitle: "Fitz's",
+  siteDescription: "Don't mind me, lol",
+  favicon: "/favicon.ico",
+  siteImagePath: "/images/sarissa.png",
+  footer: "© 1995 - 2022 Fitz's",
+  dateFormat: "dd.MM.yyyy HH:mm",
+  socialMedia: {
+    facebook: "YouShallKnowTheTruthAndTheTruthShallMakeYouMad",
+    twitter: "fitzgeral125",
+    linkedin: "fitz-geral-67bab029",
+    github: "fitzfitz",
+  },
+  pageSize: 5,
+  i18n: {
+    search: {
+      placeholder: "Search posts...",
+    },
+    archive: {
+      select: "Select Year",
+    },
+    page: "Page",
+    resultFound: " result(s) found",
+  },
+};
